8f1b6e456759467ff62ef613948685349522dce7
Jonathan Piland <jonathan.piland@duke.edu>
2021-05-09 19:50:25 -0400

    add files

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const serveIndex = require('serve-index');
+const fs = require('fs');
+
+app.get("/api/*", (req, res) => {
+    res.send("Resource requested: " + req.url.substr(5));
+})
+
+app.use('/files', serveIndex(__dirname + '/files', {
+    stylesheet: "directory-style.css",
+    icons: true
+}));
+
+app.get('/files/*', (req, res) => {
+    let path = req.url.substr(7);
+    let fileRoot = `${__dirname}/files`;
+    if (fs.existsSync(fileRoot + "/" + path)) {
+        res.sendFile(path, {root: fileRoot});
+    } else {
+        res.send(`File or directory "${path}" not found!`);
+    }
+});
+
+app.use(express.static(`${__dirname}/build`));
+
+app.use('*',  (req, res)=> {
+    console.log(req.url)
+    res.sendFile("/build/index.html", {
+        "root": __dirname
+    });
+});
+
+app.listen(80);
