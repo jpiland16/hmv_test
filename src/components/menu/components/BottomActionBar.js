06b014aa47411488ba5ec717e911582c616fff15
Jonathan Piland <jonathan.piland@duke.edu>
2021-05-10 10:10:33 -0400

    transitions still not working

--- src/components/menu/components/BottomActionBar.js
+++ src/components/menu/components/BottomActionBar.js
@@ -3,14 +3,17 @@ import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import TuneIcon from '@material-ui/icons/Tune'
 import InfoIcon from '@material-ui/icons/Info'
+import Tooltip from '@material-ui/core/Tooltip'
 import { Transition, CSSTransition } from 'react-transition-group'
 
 export default function BottomActionBar(props) {
     return (
         <div className="bottomBar">
-            <IconButton>
-                <InfoIcon />
-            </IconButton>
+            <Tooltip title="About">
+                <IconButton>
+                    <InfoIcon />
+                </IconButton>
+            </Tooltip>
             <Button>
                 <TuneIcon /> Test model
             </Button>
@@ -19,20 +22,22 @@ export default function BottomActionBar(props) {
                 timeout={300}
                 classNames='pinIcon'
             >
-                <IconButton className="pinIcon" style={{
-                    position: "absolute",
-                    right: 0,
-                }} onClick={props.onClick}>
-                    { props.pinActive ?
-                        <span className="material-icons">
-                            push_pin
-                        </span>
-                    :
-                        <span className="material-icons-outlined">
-                            push_pin
-                        </span>
-                    }
-                </IconButton>
+                <Tooltip title={props.pinActive ? "Unpin menu" : "Pin menu"}>
+                    <IconButton className="pinIcon" style={{
+                        position: "absolute",
+                        right: 0
+                    }} onClick={props.onClick}>
+                        { props.pinActive ?
+                            <span className="material-icons">
+                                push_pin
+                            </span>
+                        :
+                            <span className="material-icons-outlined">
+                                push_pin
+                            </span>
+                        }
+                    </IconButton>
+                </Tooltip>
             </CSSTransition>
         </div>
     );
--- src/components/menu/components/SideActionBar.js
+++ src/components/menu/components/SideActionBar.js
@@ -2,20 +2,25 @@ import './menu-components.css';
 import IconButton from '@material-ui/core/IconButton'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 import SettingsIcon from '@material-ui/icons/Settings'
+import Tooltip from '@material-ui/core/Tooltip'
 import Zoom from '@material-ui/core/Zoom'
 
 export default function SideActionBar(props) {
     return (
         <div className="sidebar">
             <Zoom in={props.visible} style={{ transitionDelay: props.visible ? '75ms' : '0ms' }}>
-                <IconButton>
-                    <AccountCircleIcon />
-                </IconButton>
+                <Tooltip title="My account" placement="right">
+                    <IconButton>
+                        <AccountCircleIcon />
+                    </IconButton>
+                </Tooltip>
             </Zoom>
             <Zoom in={props.visible} style={{ transitionDelay: props.visible ? '150ms' : '0ms' }}>
-                <IconButton>
-                    <SettingsIcon />
-                </IconButton>
+                <Tooltip title="Settings" placement="right">
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
+                </Tooltip>
             </Zoom>
         </div>
     );
