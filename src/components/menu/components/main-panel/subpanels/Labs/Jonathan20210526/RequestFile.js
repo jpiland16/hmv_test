04587e9045a784bea6891d945fac53c2e24d7fbe
Jonathan Piland <jonathan.piland@duke.edu>
2021-05-26 16:15:18 -0400

    add file download progress

--- src/components/menu/components/main-panel/subpanels/Labs/Jonathan20210526/RequestFile.js
+++ src/components/menu/components/main-panel/subpanels/Labs/Jonathan20210526/RequestFile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as THREE from 'three'
+import LinearProgress from '@material-ui/core/LinearProgress'
 
 const boneList = {
     'BACK': 46,
@@ -14,34 +15,47 @@ const AUTO_RIPPLE = true;
 const REPEAT = false;
 const FPS = 30;
 
+let outgoingRequest = false;
+
 export default function GeneratedData(props) {
 
+    const [ progress, setProgress ] = React.useState(0)
+
     if (props.data.length === 0) {
-        props.setUseGlobalQs(USE_GLOBAL);
-        props.setUseRipple(AUTO_RIPPLE);
-        props.setRepeat(REPEAT);
-        props.setFPS(FPS);
-        props.setLastIndex(-1);
 
-        let x = new XMLHttpRequest();
-        x.open("GET", "/files/demo/S4-ADL4.dat");
+        if(!outgoingRequest) {
+            props.setUseGlobalQs(USE_GLOBAL);
+            props.setUseRipple(AUTO_RIPPLE);
+            props.setRepeat(REPEAT);
+            props.setFPS(FPS);
+            props.setLastIndex(-1);
 
-        x.onload = () => {
-            let inputArray = x.responseText.split("\n");
-            let linesArray = [];
+            let x = new XMLHttpRequest();
+            x.open("GET", "/files/demo/S4-ADL4.dat");
 
-            for (let i = 0; i < inputArray.length - 1; i++) { // Last line is blank
-                linesArray[i] = inputArray[i].split(" ");
+            x.onload = () => {
+                let inputArray = x.responseText.split("\n");
+                let linesArray = [];
+
+                for (let i = 0; i < inputArray.length - 1; i++) { // Last line is blank
+                    linesArray[i] = inputArray[i].split(" ");
+                }
+
+                props.setData(linesArray);
+                outgoingRequest = false;
             }
 
-            props.setData(linesArray);
-        }
+            x.onprogress = (event) => {
+                setProgress(Math.round(event.loaded / event.total * 100));
+            }
 
-        x.onerror = (error) => {
-            console.log(error);
-        }
+            x.onerror = (error) => {
+                console.log(error);
+            }
 
-        x.send();
+            x.send();
+            outgoingRequest = true;
+        }
 
     }
 
@@ -63,6 +77,8 @@ export default function GeneratedData(props) {
 
     return (
         <div>
+            <div>{props.data.length > 0 ? "File downloaded successfully." : `File downloading: ${progress}% complete`}</div>
+            { props.data.length === 0 && <LinearProgress variant="determinate" value={progress} /> }
             <a href="https://github.com/jpiland16/hmv_test/blob/master/src/components/menu/components/main-panel/subpanels/Labs/Jonathan20210526/RequestFile.js" target="_blank" rel="noreferrer">View relevant code</a>
         </div>
     )
