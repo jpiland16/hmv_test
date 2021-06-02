feb678e3ae43395cf3b225bf9d7c9ff1f9c07437
Jonathan Piland <jonathan.piland@duke.edu>
2021-06-02 12:17:21 -0400

    add 404 path, dev path

--- src/App.js
+++ src/App.js
@@ -7,13 +7,25 @@ export default function App() {
   <BrowserRouter>
         <Switch>
             <Route exact path="/visualizer">
-                <Viewport />
+                <Viewport dev={false}/>
+            </Route>
+            <Route exact path="/visualizer/dev">
+                <Viewport dev={true}/>
             </Route>
             <Route exact path="/">
                 <div>Homepage</div>
                 <button onClick={() => window.location.href = "/visualizer"}>
                   Go to visualizer
                 </button>
+            </Route>
+            <Route>
+                <div>
+                    Page not found!
+                        <br />
+                        <button onClick={() => window.location.href = "/"}>
+                            Return to home page
+                        </button>
+                </div>
             </Route>
         </Switch>
     </BrowserRouter>
